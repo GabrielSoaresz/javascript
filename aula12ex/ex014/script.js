916062128accc7ae55bfd2ac1a8a8865f7926365
Gabriel Soares <gabrielsoaresp.227@gmail.com>
2024-01-15 16:03:09 -0300

    Atualização ex14

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,18 @@
-alert('Olá')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora >= 0 && hora < 12){
+        img.src = '../img/manha.png'
+        document.body.style.background = '#e4b77b'
+    }else if(hora >=12 && hora <= 18){
+        img.src = '../img/tarde.png'
+        document.body.style.background = '#FF9500'
+    }else {
+        img.src = '../img/noite.png'
+        document.body.style.background = '#2f2641'
+    }
+}
