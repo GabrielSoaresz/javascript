b05225ed3422af0399bab26c192c0cd5afc3125e
Gabriel Soares <gabrielsoaresp.227@gmail.com>
2024-01-17 09:44:25 -0300

    Atualização ex015

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -10,12 +10,33 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(form_ano.value)
         var genero = ''
+        var img = document.createElement('img') //criando imagem javascript
+        img.setAttribute('id', 'foto') // id = 'foto'
         if (fsex[0].checked){
             genero = 'Homem'
+            if(idade >= 0 && idade < 10){
+                img.setAttribute('src','menino.png')
+            }else if(idade < 21){
+                img.setAttribute('src','jovem-m.png')
+            }else if(idade < 50){
+                img.setAttribute('src','homem.png')
+            }else{
+                img.setAttribute('src','idoso.png')
+            }
         }else if (fsex[1].checked){
             genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                //criança
+            }else if(idade < 21){
+                //jovem
+            }else if(idade < 50){
+                //adulto
+            }else{
+                //idoso
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
