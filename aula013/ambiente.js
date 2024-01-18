9112ad4b0eb84f332231aeef48f463481ef1e9b8
Gabriel Soares <gabrielsoaresp.227@gmail.com>
2024-01-18 15:41:54 -0300

    Aprendendo while com javascript

---
+++ aula013/ambiente.js
@@ -0,0 +1,22 @@
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+//Do While
+var i = 1
+do {
+    console.log(`Linha ${i}`)
+    i++
+} while(i <= 6)
---
+++ aula013/node_7f78894727cbf.tmp.js
@@ -0,0 +1,21 @@
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+var i = 1
+do{
+    console.log(`Linha ${i}`)
+    c++
+}while(i <= 6)
