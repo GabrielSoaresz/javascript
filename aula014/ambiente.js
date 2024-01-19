a465cbc612e9068029d4aba108c376444ea284f2
Gabriel Soares <gabrielsoaresp.227@gmail.com>
2024-01-19 10:10:11 -0300

    criação aula014

---
+++ aula014/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+while (c <= 5){
+    console.log('Passo' + c)
+    c++ // c += 1
+}
+ var i = 1
+do{
+    console.log('Linha' + i)
+    i++
+}while(i < 5)
+
+for(var n = 1; n <= 5; n++){
+    console.log('Teste' + n)
+}
