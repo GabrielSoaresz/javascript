ce2395258d1701f6edd7f7d789641f6cb88617a6
Gabriel Soares <gabrielsoaresp.227@gmail.com>
2024-01-22 09:53:25 -0300

    Finalizando ex016

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,12 +1,37 @@
 function contar(){
-    var inicio = document.getElementById('txti')
-    var fim = document.getElementById('txtf')
-    var pular = document.getElementById('txtp')
-    var res = document.getElementById('res')
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    while(inicio <= fim){
-        res.innerHTML = inicio
-        inicio += pular
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = 'Impossivel contar'
+        window.alert('[ERRO] faltam dados!')
+    }else{
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        let c = i
+        if(p <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if(i < f){
+            //Contagem Crescente
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} 👉 `
+            }
+           
 
+        }else{
+            //Contagem Decrescente
+            while(c >= f){
+                res.innerHTML += `${c} 👉 `
+                c -= p
+
+            }
+        }
+        res.innerHTML += '🏁'
     }
 }
