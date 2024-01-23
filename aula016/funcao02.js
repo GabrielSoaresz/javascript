0b5349c6ffc948bdb729bd0c4190abeb706c2179
Gabriel Soares <gabrielsoaresp.227@gmail.com>
2024-01-23 18:44:29 -0300

    Funções Aula016

---
+++ aula016/funcao02.js
@@ -0,0 +1,8 @@
+function soma(n1=0, n2=0){ //Se o valor não for passado na chamada ele será 0
+    return n1 +n2
+}
+console.log(soma(2,5))
+console.log(soma(10,15))
+console.log(soma(300,2))
+console.log(soma(4,4))
+console.log(soma(4,0))
